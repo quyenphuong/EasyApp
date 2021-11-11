b81e5d571b65adff96f55b68554e6b3f8e39fbd1
phuongquyen <ltpquyen2000@gmail.com>
2021-11-11 16:30:44 +0700

    Design theme

--- app/src/main/java/com/example/easyapp/LoginActivity.java
+++ app/src/main/java/com/example/easyapp/LoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,21 +17,30 @@ import com.google.android.material.textfield.TextInputLayout;
 public class LoginActivity extends AppCompatActivity {
 
     ImageView image;
-    TextView signinText, callSignUp;
+    TextView signinText, callSignUp, forgetSign;
     TextInputLayout username, password;
-    Button go_btn;
+    Button goHome;
+    CheckBox rememberMe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        callSignUp=findViewById(R.id.signup_screen);
+        callSignUp = findViewById(R.id.signup_screen);
+        goHome = findViewById(R.id.gobtn);
+        image = findViewById(R.id.logoimg);
+        forgetSign = findViewById(R.id.forgotpass);
+        rememberMe = findViewById(R.id.remember);
+        signinText = findViewById(R.id.signintext);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
 
-        image=findViewById(R.id.logoimg);
-        signinText=findViewById(R.id.signintext);
-        username=findViewById(R.id.username);
-        password=findViewById(R.id.password);
-
+        goHome.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent= new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
         callSignUp.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -44,8 +54,11 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent= new Intent(LoginActivity.this, SignUpActivity.class);
         Pair[] pairs=new Pair[7];
         pairs[0]=new Pair<View, String>(image, "logo_image");
-        pairs[4]=new Pair<View, String>(username, "username_tran");
-        pairs[5]=new Pair<View, String>(password, "password_tran");
+        pairs[1]=new Pair<View, String>(signinText, "signintext");
+
+        pairs[2]=new Pair<View, String>(username, "username_tran");
+        pairs[3]=new Pair<View, String>(password, "password_tran");
+
         pairs[6]=new Pair<View, String>(callSignUp, "login_signup_tran");
 //            ActivityOptions options=makeSceneTransitionAnimation(Login_Activity.this, pairs);
 //            startActivity(intent, options.toBundle());
