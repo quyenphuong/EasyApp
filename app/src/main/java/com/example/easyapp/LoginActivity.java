6cdabea222daf9664212f81a26f9c56301a64fd0
quyenphuong <73479117+quyenphuong@users.noreply.github.com>
2021-11-14 22:31:46 +0700

    fix

--- app/src/main/java/com/example/easyapp/LoginActivity.java
+++ app/src/main/java/com/example/easyapp/LoginActivity.java
@@ -1,8 +1,7 @@
-package com.example.easyapp;
+package com.example.easyapp.ui;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.compose.ui.unit.TextUnit;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,49 +9,53 @@ import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.easyapp.R;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
-// ...
-// Initialize Firebase Auth
 
-    private ImageView image;
-    private TextView signinText, callSignUp, forgetSign;
-    private TextInputLayout edtemail, edtpassword;
-
-    private Button goHome;
-    private CheckBox rememberMe;
+    ImageView image;
+    TextView signinText, callSignUp, forgetSign;
+    TextInputLayout username, password;
+    Button goHome;
+    CheckBox rememberMe;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mAuth = FirebaseAuth.getInstance();
+
         callSignUp = findViewById(R.id.signup_screen);
         goHome = findViewById(R.id.gobtn);
         image = findViewById(R.id.logoimg);
         forgetSign = findViewById(R.id.forgotpass);
         rememberMe = findViewById(R.id.remember);
         signinText = findViewById(R.id.signintext);
-        edtemail = findViewById(R.id.email);
-        edtpassword = findViewById(R.id.password);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
 
         goHome.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                Login(edtemail.getEditText().getText().toString(),edtpassword.getEditText().getText().toString());
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+
+                Toast.makeText(LoginActivity.this,"Đăng nhập thành công",Toast.LENGTH_SHORT).show();
+                intent = new Intent(LoginActivity.this, HomeActivity.class);
+
+                startActivity(intent);
 
             }
         });
@@ -60,48 +63,25 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                register();
+                show();
             }
         });
-
     }
 
-    public void register(){
-        Intent intent= new Intent(LoginActivity.this, SignUpActivity.class);
-        Pair[] pairs=new Pair[7];
-        pairs[0]=new Pair<View, String>(image, "logo_image");
-        pairs[1]=new Pair<View, String>(signinText, "signintext");
+    public void show() {
+        Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+        Pair[] pairs = new Pair[7];
+        pairs[0] = new Pair<View, String>(image, "logo_image");
+        pairs[1] = new Pair<View, String>(signinText, "signintext");
 
-        pairs[2]=new Pair<View, String>(edtemail, "email_tran");
-        pairs[3]=new Pair<View, String>(edtpassword, "password_tran");
+        pairs[2] = new Pair<View, String>(username, "username_tran");
+        pairs[3] = new Pair<View, String>(password, "password_tran");
 
-        pairs[6]=new Pair<View, String>(callSignUp, "login_signup_tran");
+        pairs[6] = new Pair<View, String>(callSignUp, "login_signup_tran");
 //            ActivityOptions options=makeSceneTransitionAnimation(Login_Activity.this, pairs);
 //            startActivity(intent, options.toBundle());
         startActivity(intent);
     }
 
-    private void Login(String Email,String Password) {
 
-        mAuth.signInWithEmailAndPassword(Email, Password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText(LoginActivity.this, "Đăng nhập thành công.",
-                                    Toast.LENGTH_SHORT).show();
-                            Intent intent= new Intent(LoginActivity.this, HomeActivity.class);
-                            startActivity(intent);
-                        } else {
-                            // If sign in fails, display a message to the user.
-
-                            Toast.makeText(LoginActivity.this, "Đăng nhập thất bại.",
-                                    Toast.LENGTH_SHORT).show();
-
-                        }
-                    }
-                });
-    }
 }
