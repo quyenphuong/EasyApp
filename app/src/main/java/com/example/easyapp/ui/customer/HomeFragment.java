7d681e2732a4bd92d8b3e8f7218495c2bef9fbcb
phuongquyen <ltpquyen2000@gmail.com>
2021-11-25 22:25:22 +0700

    Feature

--- app/src/main/java/com/example/easyapp/ui/customer/HomeFragment.java
+++ app/src/main/java/com/example/easyapp/ui/customer/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.easyapp.ui.customer;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,10 +15,16 @@ import android.view.ViewGroup;
 import com.example.easyapp.R;
 import com.example.easyapp.adapters.HomeHorAdapter;
 import com.example.easyapp.adapters.HomeVerAdapter;
+import com.example.easyapp.feature.ClubActivity;
+import com.example.easyapp.feature.GiftActivity;
 import com.example.easyapp.feature.Iclickitemrecycler;
+import com.example.easyapp.feature.MoveActivity;
 import com.example.easyapp.feature.ShippingActivity;
+import com.example.easyapp.feature.VoucherActivity;
 import com.example.easyapp.model.HomeHorModel;
 import com.example.easyapp.model.HomeVerModel;
+import com.example.easyapp.ui.ForgotActivity;
+import com.example.easyapp.ui.LoginActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,42 +32,98 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    RecyclerView homeHorizontalRec, homeVerticalRec;
-    List<HomeHorModel> homeHorModelList;
-    HomeHorAdapter homeHorAdapter;
+    private RecyclerView  homeVerticalRec;
+    private List<HomeHorModel> homeHorModelList;
+    private HomeHorAdapter homeHorAdapter;
+    private CardView cardviewShip, cardviewMove, cardviewVoucher, cardviewGift, cardviewClub;
+
 
     //vertical
-    List<HomeVerModel>homeVerModelList;
-    HomeVerAdapter homeVerAdapter;
+    private List<HomeVerModel>homeVerModelList;
+    private HomeVerAdapter homeVerAdapter;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        homeHorizontalRec = root.findViewById(R.id.home_hor_rec);
+
+//        homeHorizontalRec = root.findViewById(R.id.home_hor_rec);
         homeVerticalRec = root.findViewById(R.id.home_ver_rec);
+
+
+        cardviewShip = root.findViewById(R.id.cardview_Ship);
+        cardviewShip.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getActivity(), ShippingActivity.class);
+                startActivity(intent);
+            }
+        });
+        //////////
+        cardviewMove = root.findViewById(R.id.cardview_Move);
+        cardviewMove.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getActivity(), MoveActivity.class);
+                startActivity(intent);
+            }
+        });
+        //////
+        //////////
+        cardviewVoucher = root.findViewById(R.id.cardview_Voucher);
+        cardviewVoucher.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getActivity(), VoucherActivity.class);
+                startActivity(intent);
+            }
+        });
+        //////////
+        cardviewGift = root.findViewById(R.id.cardview_Gift);
+        cardviewGift.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getActivity(), GiftActivity.class);
+                startActivity(intent);
+            }
+        });
+        //////////
+        cardviewClub = root.findViewById(R.id.cardview_Club);
+        cardviewClub.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getActivity(), ClubActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         //Horizontal RecyclerView
 
-        homeHorizontalRec.setLayoutManager(new LinearLayoutManager(getActivity()));
+//        homeHorizontalRec.setLayoutManager(new LinearLayoutManager(getActivity()));
         homeHorModelList = new ArrayList<>();
         homeHorModelList.add(new HomeHorModel(R.drawable.ship, "Gửi Hàng"));
         homeHorModelList.add(new HomeHorModel(R.drawable.voucher, "Ưu đãi"));
         homeHorModelList.add(new HomeHorModel(R.drawable.car, "Di chuyển"));
         homeHorModelList.add(new HomeHorModel(R.drawable.gift, "Quà tặng"));
 
-        homeHorAdapter = new HomeHorAdapter(getActivity().getApplicationContext(), homeHorModelList, new Iclickitemrecycler() {
-            @Override
-            public void onclickItemfeatures(HomeHorModel homeHorModel) {
-                onClickGotoShipping(homeHorModel);
-            }
-        });
-        homeHorAdapter.notifyDataSetChanged();
-        homeHorizontalRec.setAdapter(homeHorAdapter);
-
-        homeHorizontalRec.setHasFixedSize(true);
-        homeHorizontalRec.setNestedScrollingEnabled(false);
+//        homeHorAdapter = new HomeHorAdapter(getActivity().getApplicationContext(), homeHorModelList, new Iclickitemrecycler() {
+//            @Override
+//            public void onclickItemfeatures(HomeHorModel homeHorModel) {
+//                onClickGotoShipping(homeHorModel);
+//            }
+//        });
+//        homeHorAdapter.notifyDataSetChanged();
+//        homeHorizontalRec.setAdapter(homeHorAdapter);
+//
+//        homeHorizontalRec.setHasFixedSize(true);
+//        homeHorizontalRec.setNestedScrollingEnabled(false);
 
         //Vertical RecyclerView
         homeVerModelList = new ArrayList<>();
@@ -77,15 +140,15 @@ public class HomeFragment extends Fragment {
         homeVerticalRec.setNestedScrollingEnabled(false);
         homeVerticalRec.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false));
 
-        homeHorizontalRec.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+//        homeHorizontalRec.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
         // Inflate the layout for this fragment
 
         return root;
     }
-    private void onClickGotoShipping(HomeHorModel homeHorModel){
-        Intent intent = new Intent(getActivity(), ShippingActivity.class);
-        Bundle bundle = new Bundle();
-        intent.putExtras(bundle);
-        startActivity(intent);
-    }
+//    private void onClickGotoShipping(HomeHorModel homeHorModel){
+//        Intent intent = new Intent(getActivity(), ShippingActivity.class);
+//        Bundle bundle = new Bundle();
+//        intent.putExtras(bundle);
+//        startActivity(intent);
+//    }
 }
