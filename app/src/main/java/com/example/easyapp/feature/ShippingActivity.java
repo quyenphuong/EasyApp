abfd53873577d1210e35e5dc9cfe9aade9cce310
phuongquyen <ltpquyen2000@gmail.com>
2021-11-27 13:48:54 +0700

    diaglog UI

--- app/src/main/java/com/example/easyapp/feature/ShippingActivity.java
+++ app/src/main/java/com/example/easyapp/feature/ShippingActivity.java
@@ -3,6 +3,8 @@ package com.example.easyapp.feature;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.easyapp.R;
@@ -11,10 +13,22 @@ import java.util.List;
 
 public class ShippingActivity extends AppCompatActivity {
 
+
+    EditText addressHome, addressShip;
+    Button price;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shipping);
+
+        //////////
+        addressHome = findViewById(R.id.address_home);
+        addressShip = findViewById(R.id.address_ship);
+        price = findViewById(R.id.price_temp);
+
+        /////////
+
+
 
     }
 }
